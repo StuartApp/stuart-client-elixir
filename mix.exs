0b6ee56f97c6a8e7ec8c31388bcdfcac33203ed1
Maximilien Tyc <maximilien.tyc@gmail.com>
2018-08-01 12:48:22 +0200

    add README

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule StuartClientElixir.MixProject do
   def project do
     [
       app: :stuart_client_elixir,
-      version: "0.1.0",
+      version: "1.0.0",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       deps: deps()
