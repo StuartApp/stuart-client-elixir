4c0a72ba4414a79a87369cda394879a25b750597
Jaime Iniesta <jaimeiniesta@gmail.com>
2018-08-02 13:23:32 +0200

    replaces Poison with Jason as it's faster

--- mix.exs
+++ mix.exs
@@ -34,7 +34,7 @@ defmodule StuartClientElixir.MixProject do
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
       {:httpoison, "~> 0.13.0"},
       {:oauth2, "~> 0.9"},
-      {:poison, "~> 4.0"},
+      {:jason, "~> 1.1"},
       {:cachex, "~> 3.0"},
       {:mox, "~> 0.3", only: :test},
       {:mock, "~> 0.3.1", only: :test}
