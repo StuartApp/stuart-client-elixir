e9034a18058e77dd5969d7680ea0c1761f9e50ad
Maximilien Tyc <maximilien.tyc@gmail.com>
2018-08-16 16:42:04 +0200

    add queue mechanism

---
+++ lib/stuart_client_elixir/infrastructure/queue.ex
@@ -0,0 +1,26 @@
+defmodule StuartClientElixir.Infrastructure.Queue do
+  use GenServer
+
+  def start_link() do
+    GenServer.start_link(__MODULE__, :queue.new(), name: :stuart_queue)
+  end
+
+  def init(queue) do
+    {:ok, queue}
+  end
+
+  def enqueue(item), do: GenServer.cast(:stuart_queue, {:enqueue, item})
+
+  def unqueue(), do: GenServer.call(:stuart_queue, :unqueue)
+
+  # callbacks
+  def handle_call(:unqueue, _from, queue) do
+    {item, q} = :queue.out(queue)
+    case item do
+      {:value, value} -> {:reply, value, q}
+      :empty -> {:reply, nil, q}
+    end
+  end
+
+  def handle_cast({:enqueue, item}, queue), do: {:noreply, :queue.in(item, queue)}
+end
---
+++ test/stuart_client_elixir/infrastructure/queue_test.exs
@@ -0,0 +1,35 @@
+defmodule StuartClientElixirTest.Infrastructure.QueueTest do
+  use ExUnit.Case
+
+  alias StuartClientElixir.Infrastructure.Queue
+
+  test "unqueue on an empty queue should return nil" do
+    assert nil == Queue.unqueue()
+  end
+
+  test "unqueue on a queue will one item should return the item" do
+    # given
+    enqueued_value = :test
+    Queue.enqueue(enqueued_value)
+
+    # when
+    unqueued_value = Queue.unqueue()
+
+    # then
+    assert enqueued_value == unqueued_value
+  end
+
+  test "unqueue items first in first out" do
+    # given
+    Queue.enqueue(:first)
+    Queue.enqueue(:second)
+
+    # when
+    first = Queue.unqueue
+    second = Queue.unqueue
+
+    # then
+    assert first == :first
+    assert second == :second
+  end
+end
