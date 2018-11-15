b865a426cb8d43ddd6ee6435d35249a16f37a6de
Jaime Iniesta <jaimeiniesta@gmail.com>
2018-11-15 11:28:43 +0100

    Return the same response format for OAuth errors.

--- test/stuart_client_elixir/http_client_test.exs
+++ test/stuart_client_elixir/http_client_test.exs
@@ -4,12 +4,22 @@ defmodule StuartClientElixirTest.HttpClientTest do
   import Mock
   alias StuartClientElixir.{Authenticator, HttpClient, Environment, Credentials}
 
+  @good_credentials %Credentials{
+    client_id: "good-id",
+    client_secret: "good-secret"
+  }
+
+  @bad_credentials %Credentials{
+    client_id: "bad-id",
+    client_secret: "bad-secret"
+  }
+
   setup_with_mocks([
     {
       Authenticator,
       [],
       [
-        access_token: fn _environment, _credentials -> {:ok, "sample-access-token"} end
+        access_token: fn _environment, credentials -> authenticator_response(credentials) end
       ]
     },
     {
@@ -17,10 +27,12 @@ defmodule StuartClientElixirTest.HttpClientTest do
       [],
       [
         get: fn _, _ ->
-          {:ok, %HTTPoison.Response{status_code: 201, body: Jason.encode!(%{sample: "response"})}}
+          {:ok,
+           %HTTPoison.Response{status_code: 201, body: Jason.encode!(%{sample: "get response"})}}
         end,
         post: fn _, _, _ ->
-          {:ok, %HTTPoison.Response{status_code: 201, body: Jason.encode!(%{sample: "response"})}}
+          {:ok,
+           %HTTPoison.Response{status_code: 201, body: Jason.encode!(%{sample: "post response"})}}
         end
       ]
     }
@@ -30,13 +42,10 @@ defmodule StuartClientElixirTest.HttpClientTest do
 
   describe "get" do
     test "calls HTTPoison with correct parameters" do
-      # given
-      HttpClient.get("/sample-endpoint", %{
-        environment: Environment.sandbox(),
-        credentials: sample_credentials()
-      })
+      expected_response = %{body: %{"sample" => "get response"}, status_code: 201}
 
-      # then
+      assert HttpClient.get("/sample-endpoint", config()) == expected_response
+
       assert called(
                HTTPoison.get(
                  "https://sandbox-api.stuart.com/sample-endpoint",
@@ -44,14 +53,21 @@ defmodule StuartClientElixirTest.HttpClientTest do
                )
              )
     end
+
+    test "returns explicit error when authentication fails" do
+      expected_response = %{body: %{"error" => "Bad credentials"}, status_code: 401}
+
+      assert HttpClient.get("/sample-endpoint", config(@bad_credentials)) == expected_response
+    end
   end
 
   describe "post" do
     test "calls HTTPoison with correct parameters" do
-      # when
-      HttpClient.post("/sample-endpoint", sample_request_body(), config())
+      expected_response = %{body: %{"sample" => "post response"}, status_code: 201}
 
-      # then
+      assert HttpClient.post("/sample-endpoint", sample_request_body(), config()) ==
+               expected_response
+
       assert called(
                HTTPoison.post(
                  "https://sandbox-api.stuart.com/sample-endpoint",
@@ -60,17 +76,25 @@ defmodule StuartClientElixirTest.HttpClientTest do
                )
              )
     end
+
+    test "returns explicit error when authentication fails" do
+      expected_response = %{body: %{"error" => "Bad credentials"}, status_code: 401}
+
+      assert HttpClient.post("/sample-endpoint", sample_request_body(), config(@bad_credentials)) ==
+               expected_response
+    end
   end
 
   #####################
   # Private functions #
   #####################
 
-  defp sample_credentials do
-    %Credentials{
-      client_id: "sample-client-id",
-      client_secret: "sample-client-secret"
-    }
+  defp authenticator_response(@good_credentials) do
+    {:ok, "sample-access-token"}
+  end
+
+  defp authenticator_response(@bad_credentials) do
+    {:error, %OAuth2.Response{status_code: 401, body: %{"error" => "Bad credentials"}}}
   end
 
   defp sample_request_body, do: Jason.encode!(%{sample: "request"})
@@ -82,9 +106,9 @@ defmodule StuartClientElixirTest.HttpClientTest do
       "Content-Type": "application/json"
     ]
 
-  defp config,
+  defp config(credentials \\ @good_credentials),
     do: %{
       environment: Environment.sandbox(),
-      credentials: sample_credentials()
+      credentials: credentials
     }
 end
