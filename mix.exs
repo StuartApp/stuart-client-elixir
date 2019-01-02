ef9829d73fc548dab507ab1416c53ebef14dc071
Jaime Iniesta <jaimeiniesta@gmail.com>
2019-01-02 11:36:23 +0100

    upgrade dependencies (#15)

--- mix.exs
+++ mix.exs
@@ -32,9 +32,9 @@ defmodule StuartClientElixir.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
-      {:httpoison, "~> 1.3.1"},
-      {:oauth2, "~> 0.9.3"},
-      {:jason, "~> 1.1.1"},
+      {:httpoison, "~> 1.5"},
+      {:oauth2, "~> 0.9.4"},
+      {:jason, "~> 1.1.2"},
       {:cachex, "~> 3.1"},
       {:mox, "~> 0.4.0", only: :test},
       {:mock, "~> 0.3.2", only: :test}
