652e07b1c5fe3c8c18684ecefe57695be69e7f31
Victor Viruete <v.viruete@stuart.com>
2021-01-20 14:12:46 +0100

    version bump 1.2.1

--- mix.exs
+++ mix.exs
@@ -11,7 +11,7 @@ defmodule StuartClientElixir.MixProject do
           "GitHub" => "https://github.com/StuartApp/stuart-client-elixir"
         }
       },
-      version: "1.2.0",
+      version: "1.2.1",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
       deps: deps()
